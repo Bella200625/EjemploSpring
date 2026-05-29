5cc97bdef1bad8e069d92d7628ab15d1813eaf1b
Bella200625 <bbotellom@unicartagena.edu.co>
2026-05-29 03:20:25 -0500

    Guía 6: Intercambio de Objetos Usuario

--- src/main/java/com/example/EjemploSpring/ControladorInicio.java
+++ src/main/java/com/example/EjemploSpring/ControladorInicio.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import com.example.EjemploSpring.modelo.Usuario;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ControladorInicio {
 
     @Value("${index.mensaje}")    
-    private String dato;
+    String dato;
 
     @GetMapping("/")
     public String inicio(Model modelo) {
@@ -21,6 +22,14 @@ public class ControladorInicio {
         modelo.addAttribute("mensaje", mensaje);
         modelo.addAttribute("dato", dato);
         
+        Usuario u = new Usuario();
+        u.setCedula("1234");
+        u.setClave("Abcd");
+        u.setNombre("JOHN CARLOS ARRIETA ARRIETA");
+        u.setEmail("jarrieta@hotmail.com");
+        
+        modelo.addAttribute("alguien", u);
+
         log.info("Ejecutando el controlador inicio MVC");
         // Retorna la vista llamada "index.html"
         return "index";
---
+++ src/main/java/com/example/EjemploSpring/modelo/Usuario.java
@@ -0,0 +1,11 @@
+package com.example.EjemploSpring.modelo;
+
+import lombok.Data;
+
+@Data
+public class Usuario {
+    private String cedula;
+    private String clave;
+    private String nombre;
+    private String email;
+}
